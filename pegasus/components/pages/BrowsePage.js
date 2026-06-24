537f98bf4db1b06d600948cab146b5c33dbeb53a
bogdanmironov67 <mironovbogdan1354@gmail.com>
2026-06-24 15:45:36 +0300

    feat: use chevron icons for browse pagination

--- pegasus/components/pages/BrowsePage.js
+++ pegasus/components/pages/BrowsePage.js
@@ -284,14 +284,18 @@ export default function BrowsePage({ projectType, initialState = null, initialDa
 
         return (
             <div className="pagination-controls" style={style}>
-                <button className="button button--size-m button--type-secondary" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} aria-disabled={currentPage === 1}>
-                    {t("previous")}
+                <button className="button button--size-m button--type-secondary button--icon-only" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} aria-disabled={currentPage === 1} aria-label={t("previous")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left-icon lucide-chevron-left">
+                        <path d="m15 18-6-6 6-6"/>
+                    </svg>
                 </button>
 
                 {getPageButtons()}
 
-                <button className="button button--size-m button--type-secondary" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} aria-disabled={currentPage === totalPages}>
-                    {t("next")}
+                <button className="button button--size-m button--type-secondary button--icon-only" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} aria-disabled={currentPage === totalPages} aria-label={t("next")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-right-icon lucide-chevron-right">
+                        <path d="m9 18 6-6-6-6"/>
+                    </svg>
                 </button>
             </div>
         );
@@ -361,6 +365,7 @@ export default function BrowsePage({ projectType, initialState = null, initialDa
                 ) : (
                     <div className="subsite-empty-feed">
                         <img src="/images/kweebec.png" style={{ width: "200px" }} />
+
                         <p className="subsite-empty-feed__title">{t("noProjects")}</p>
                     </div>
                 )}
