d8317121d35ae4c0c031018c626c7313fc63cf8d
bogdanmironov67 <mironovbogdan1354@gmail.com>
2026-06-29 18:31:22 +0300

    feat: add profile cover edit button

--- pegasus/components/pages/ProfilePage.js
+++ pegasus/components/pages/ProfilePage.js
@@ -244,15 +244,15 @@ export default function ProfilePage({ user, isBanned, isSubscribed: initialSubsc
                                     <div className="profile-hero__cover" aria-hidden="true" />
                                 )}
 
-                                {isOwnProfile && !authorCover && (
-                                    <button className="button button--size-m button--type-minimal button--with-icon subsite-cover-editor__centered" type="button" onClick={() => coverInputRef.current?.click()}>
+                                {isOwnProfile && (
+                                    <button className={`button button--size-m button--type-minimal button--with-icon subsite-cover-editor__centered ${authorCover ? "subsite-cover-editor__centered--hidden" : ""}`} type="button" onClick={() => coverInputRef.current?.click()}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-image-icon lucide-image">
                                             <rect width="18" height="18" x="3" y="3" rx="2" ry="2"></rect>
                                             <circle cx="9" cy="9" r="2"></circle>
                                             <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"></path>
                                         </svg>
                                         
-                                        {t("addCover")}
+                                        {t(authorCover ? "editCover" : "addCover")}
                                     </button>
                                 )}
                             </div>
