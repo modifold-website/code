2380f37b9a72576351b1b992327050d87ac422f1
bogdanmironov67 <mironovbogdan1354@gmail.com>
2026-08-26 00:16:38 +0300

    fix: update gallery video play icon

--- pegasus/components/project/settings/GallerySettings.js
+++ pegasus/components/project/settings/GallerySettings.js
@@ -667,8 +667,8 @@ export default function GallerySettings({ project, authToken }) {
 
 											{isVideo ? (
 												<span className="gallery-settings-card__play" aria-hidden="true">
-													<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
-														<path d="m8 5 11 7-11 7V5Z" />
+													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-play-icon lucide-play">
+														<path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"/>
 													</svg>
 												</span>
 											) : null}
