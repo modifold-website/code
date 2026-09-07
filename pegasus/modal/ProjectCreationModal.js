5dbbc99f961f8a0b0db7bb62a84f52cae3b0faa2
bogdanmironov67 <mironovbogdan1354@gmail.com>
2026-09-08 01:06:05 +0300

    fix: update project import link text

--- pegasus/modal/ProjectCreationModal.js
+++ pegasus/modal/ProjectCreationModal.js
@@ -142,6 +142,11 @@ export default function ProjectCreationModal({ isOpen, authToken, onRequestClose
                             </div>
 
                             <button className="project-creation__import" type="button" onClick={() => setIsImportOpen(true)} disabled={loading}>
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
+									<path d="M12 3v12" />
+									<path d="m8 11 4 4 4-4" />
+									<path d="M8 5H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-4" />
+								</svg>
                                 {t("importFromCurseForge")}
                             </button>
                         </form>
